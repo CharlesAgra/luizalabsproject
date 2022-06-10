65edcc57c569a6a729909b15eca8b416ea768844
Charles Agra <csagra216@gmail.com>
2022-06-09 23:28:34 -0300

    a little change in the app.js

--- src/app.js
+++ src/app.js
@@ -3,5 +3,5 @@ require('dotenv').config()
 const http = require('http');
 const router = require('./routes/index');
 const server = http.createServer(async (request, response) => router.routes(request, response));
-server.listen(process.env.APP_PORT || 3000, () => console.log(`Server started on port ${process.env.APP_PORT || 3000}`));
+server.listen(process.env.APP_PORT || 9000, () => console.log(`Server started on port ${process.env.APP_PORT || 9000}`));
 
